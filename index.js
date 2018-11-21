da154f3e8016b19911705a31809a45d76bae7dcc
Danijel Beljan <danijel.beljan@icloud.com>
2018-11-21 19:41:22 +0100

    Customized some rules

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ module.exports = {
     },
     extends: 'eslint-config-airbnb-base',
     rules: {
+        "indent": ["error", 4, {'ignoreComments': true}],
+        "comma-dangle": ["error", "never"],
+        "max-len": ["error", 500],
+        "object-shorthand": ["error", "never"],
+        "no-underscore-dangle": ["error", { "allowAfterThis": true, "allowAfterSuper": true }],
+        "prefer-destructuring": [0],
+        "no-plusplus": [0]
     }
 };
