c1dc82b91cc8681b99c7a42c75f60f8126431db5
Danijel Beljan <danijel.beljan@icloud.com>
2018-11-27 10:24:10 +0100

    Disable no console rule

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports = {
     rules: {
         "arrow-parens": ["error", "as-needed"],
         "class-methods-use-this": [0],
-        "indent": ["error", 4, {'ignoreComments': true}],
         "comma-dangle": [0],
+        "indent": ["error", 4, {'ignoreComments': true}],
         "max-len": ["error", 500],
-        "object-shorthand": [0],
+        "no-console": [0],
+        "no-plusplus": [0],
         "no-underscore-dangle": ["error", { "allowAfterThis": true, "allowAfterSuper": true }],
-        "prefer-destructuring": [0],
-        "no-plusplus": [0]
+        "object-shorthand": [0],
+        "prefer-destructuring": [0]
     }
 };
